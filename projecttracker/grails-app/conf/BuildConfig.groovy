9c9b3d879481140bb69919dd080b3583135c6c3d
Ejnar Åkerman <ejnar.akerman@cybercom.com>
2014-03-20 00:12:29 +0100

    Update ignore

--- projecttracker/grails-app/conf/BuildConfig.groovy
+++ projecttracker/grails-app/conf/BuildConfig.groovy
@@ -41,9 +41,12 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenCentral()
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
+		mavenRepo "http://repository.codehaus.org/"
+		mavenRepo "http://repo.grails.org/grails/core"
+		mavenRepo "https://oss.sonatype.org/content/repositories/releases/"
     }
 
     dependencies {
@@ -59,6 +62,8 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.0.2"
         compile ':cache:1.1.1'
+		
+		compile ":rest:0.8"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
--- projecttracker/grails-app/controllers/projecttracker/ProjectController.groovy
+++ projecttracker/grails-app/controllers/projecttracker/ProjectController.groovy
@@ -1,8 +1,19 @@
 package projecttracker
 
+
 class ProjectController {
 	def scaffold = true
 	def index() { }
+	
+	
+//	def client = new RESTClient( 'http://www.acme.com/' )
+//	def resp = client.get( path : 'products/3322' ) // ACME boomerang
+//	
+//	assert resp.status == 200  // HTTP response code; 404 means not found, etc.
+//	println resp.getData()
+	
+	
+	
 //    def index = { redirect (action: current)}
 //	def current = { 
 //		//def projectName = "Project tracker"
@@ -13,4 +24,18 @@ class ProjectController {
 //	}
 //	
 //	def overdue = { render "overdue" }
+	
+	
+	
+//	void getREST() {
+//		
+//			def r = new RESTClient(url)
+//			def response = r.get(path:'test',headers:['Authorization': "auth"] )
+//			println "RESPONSE"
+//			println response.headers
+//			response.headers.each { it ->
+//				println it
+//			}
+//		
+//	}
 }
